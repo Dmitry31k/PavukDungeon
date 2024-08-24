39336ddaa6d562491aec50aa80211087827c4408
Dmitry31k <proflorik2004k@gmail.com>
2024-08-24 21:59:27 +0200

    Implementing new enemy

---
+++ Source/PavukDungeon/DroneWithLaser.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DroneWithLaser.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "PlayerPavuk.h"
+#include "Kismet/GameplayStatics.h"
+
+ADroneWithLaser::ADroneWithLaser()
+{
+
+}
+
+void ADroneWithLaser::BeginPlay()
+{
+    Super::BeginPlay();
+
+
+}
+
+void ADroneWithLaser::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    FVector StartDrawingLineLocation = ProjectileSpawnPoint->GetComponentLocation();
+    FVector EndDrawingLineLocation = StartDrawingLineLocation + LaserLength * GetActorForwardVector();
+
+    GetWorld()->LineTraceSingleByChannel(LineTraceHitResult, StartDrawingLineLocation, EndDrawingLineLocation, ECollisionChannel::ECC_Pawn);
+
+    DrawDebugLine(GetWorld(), StartDrawingLineLocation, EndDrawingLineLocation, FColor::Red);
+
+    PlayerPavuk = Cast<APlayerPavuk>(LineTraceHitResult.GetActor());
+
+    if (PlayerPavuk)
+    {
+        UGameplayStatics::ApplyDamage(PlayerPavuk, LaserDamage, GetInstigatorController(), this, UDamageType::StaticClass());
+    }
+}
---
+++ Source/PavukDungeon/DroneWithLaser.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseDrone.h"
+#include "DroneWithLaser.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAVUKDUNGEON_API ADroneWithLaser : public ABaseDrone
+{
+	GENERATED_BODY()
+	
+public:
+	
+	ADroneWithLaser();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	float LaserLength = 500;
+
+	UPROPERTY(EditDefaultsOnly)
+	float LaserDamage = 100;
+
+	FHitResult LineTraceHitResult;
+	class APlayerPavuk* PlayerPavuk;
+};

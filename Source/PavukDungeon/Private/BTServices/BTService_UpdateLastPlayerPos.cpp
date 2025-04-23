0acdd7440b5f114227087f40f86d611ac7941f71
Dmitry31k <proflorik2004k@gmail.com>
2025-04-23 10:07:49 +0200

    Perf: Moved player pawn acquisition from TickNode to OnSearchStart

--- Source/PavukDungeon/Private/BTServices/BTService_UpdateLastPlayerPos.cpp
+++ Source/PavukDungeon/Private/BTServices/BTService_UpdateLastPlayerPos.cpp
@@ -8,15 +8,23 @@
 UBTService_UpdateLastPlayerPos::UBTService_UpdateLastPlayerPos()
 {
     NodeName = "Update last known player location";
+
+    bCreateNodeInstance = true;
+    bNotifyTick = true;
+}
+
+void UBTService_UpdateLastPlayerPos::OnSearchStart(FBehaviorTreeSearchData& SearchData)
+{
+    Super::OnSearchStart(SearchData);
+
+    PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
 }
 
 void UBTService_UpdateLastPlayerPos::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
     Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
-
-    if (PlayerPawn)
+    if (PlayerPawn.IsValid())
     {
         OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), PlayerPawn->GetActorLocation());
     }
